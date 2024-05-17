069c52d78f7271bb4aa55479c6b7007612db1642
Leandro Menezes <lmsilv@gmail.com>
2024-05-17 16:32:36 -0300

    Criação do método `findAll`.

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -2,7 +2,9 @@ package br.com.erudio.controllers;
 
 import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,12 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return this.service.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id) throws Exception {
         return this.service.findById(id);
     }
--- src/main/java/br/com/erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,6 +1,8 @@
 package br.com.erudio.services;
 
 import br.com.erudio.model.Person;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,24 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding people...");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
     
     public Person findById(String id) {
         logger.info("Finding one person...");
         Person person = new Person(counter.incrementAndGet(), "Leandro", "Silva", "Samambaia, Brasília, Brasil", "Male");
         return person;
+    }
+
+    private Person mockPerson(int i) {
+        return new Person(counter.incrementAndGet(), "A Unknown", "Person " + i , "Some where in Brasil", "Male");
     }
 }
